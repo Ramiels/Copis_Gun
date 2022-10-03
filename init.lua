602c92f0ec3ac0bb9d51493d423f418b5bf6f0a6
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-10-04 07:58:21 +1100

    formatting

--- init.lua
+++ init.lua
@@ -3,16 +3,13 @@ ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/copis_gun/files/scri
 
 
 function OnPlayerSpawned( player_entity ) -- This runs when player entity has been created
-
-	if GameHasFlagRun("copis_gun_spawned") ~= true then
-
+	if not GameHasFlagRun("copis_gun_spawned") then
         -- most based akimbo shooter sim
 		if ModIsEnabled("Twin-Linked") then
 			local x, y = EntityGetTransform(player_entity)
 			EntityLoad("mods/copis_gun/files/entities/items/glock.xml", x + 20, y - 5)
 			GamePrintImportant("The gods grant you a second gun!", "make sure to unequip the offhand gun when restocking!", "mods/copis_gun/files/ui_gfx/decoration/twin.png")
 		end
-
 		GameAddFlagRun("copis_gun_spawned")
 	end
 end
