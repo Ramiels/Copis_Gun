02dc77bf422f2ed664cb08de7a78e39dbea22fd8
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-10-04 08:01:22 +1100

    optimise scale_update.lua

--- files/scripts/scale_update.lua
+++ files/scripts/scale_update.lua
@@ -1,10 +1,6 @@
 local entity_id = GetUpdatedEntityID()
 local _, _, rad = EntityGetTransform(entity_id)
 local sprite_comp = EntityGetComponentIncludingDisabled(entity_id, "SpriteComponent", "item")
-for _, value in ipairs(sprite_comp) do
-    if rad >= -math.pi / 2 and rad <= math.pi / 2 then
-        ComponentSetValue2(value, "special_scale_y", 0.4)
-    else
-        ComponentSetValue2(value, "special_scale_y", -0.4)
-    end
+for i=1, #sprite_comp do
+    ComponentSetValue2(sprite_comp[i], "special_scale_y", (rad >= -math.pi / 2 and rad <= math.pi / 2) and 0.4 or -0.4)
 end
