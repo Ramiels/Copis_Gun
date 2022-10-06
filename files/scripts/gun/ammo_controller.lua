c943d408cabdc6fff3e117ec242600d93f8a0ee1
Human <54731850+Ramiels@users.noreply.github.com>
2022-10-06 16:52:27 -0400

    broken display

--- files/scripts/gun/ammo_controller.lua
+++ files/scripts/gun/ammo_controller.lua
@@ -1,10 +1,11 @@
 dofile_once("mods/copis_gun/files/scripts/lib/copi_util.lua")
 
-local props = get_data_from_vsc(entity_id)
+local entity_id = GetUpdatedEntityID()
 local ammo = getAmmoManager(entity_id)
 local recharge = true
+
 -- Don't recharge while shooting attribute
-if props.ammo_system_recharge_while_shooting ~= true then
+if ammo.ammo_system_recharge_while_shooting ~= true then
     local shooterControls = EntityGetComponent(EntityGetRootEntity(GetUpdatedEntityID()), "ControlsComponent")
     if shooterControls then
         if ComponentGetValue2(shooterControls, "mButtonDownFire") then
@@ -14,11 +15,11 @@ if props.ammo_system_recharge_while_shooting ~= true then
 end
 
 -- Ammo recharge time attribute
-if GameGetFrameNum() % props.ammo_system_recharge_time ~= 0 then
+if GameGetFrameNum() % ammo.ammo_system_recharge_time ~= 0 then
     recharge = false
 end
 
 -- Add ammo if possible
 if recharge then
-    ammo.ammo_system_remaining = math.min(props.ammo_system_remaining + 1, props.ammo_system_capacity)
+    ammo.ammo_system_remaining = math.min(ammo.ammo_system_remaining + 1, ammo.ammo_system_capacity)
 end
