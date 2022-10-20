c82102140bb3aeda2ad16e06bd69b7fc1a7379ce
Human <54731850+Ramiels@users.noreply.github.com>
2022-10-20 00:40:38 -0400

    stuff

--- files/scripts/gun/actions.lua
+++ files/scripts/gun/actions.lua
@@ -24,12 +24,15 @@ local to_insert = {
             if (recursion_level or iteration) ~= nil then return; end
 
             local card = CurrentCard()
-            local ammo = CardGetAmmo(card)
-            if (ammo > 0) then
+            local ammo = getAmmoManager(card)
+            if (ammo.ammo_system_remaining > 0) then
                 add_projectile("mods/copis_gun/files/entities/projectiles/silver_bullet.xml")
                 c.screenshake = c.screenshake + 0.1
                 c.damage_critical_chance = c.damage_critical_chance + 20
-                CardSetAmmo(card, ammo - 1)
+                ammo.ammo_system_remaining = ammo.ammo_system_remaining - 1
+            else
+                local x, y = EntityGetTransform(GetUpdatedEntityID())
+                GamePlaySound( "mods/copis_gun/files/audio/Copis_Gun.bank", "9mm/blank", x, y )
             end
         end,
     },
@@ -55,16 +58,107 @@ local to_insert = {
             end
             if (recursion_level or iteration) ~= nil then return; end
 
-            local card = CurrentCard()
-            local ammo = CardGetAmmo(card)
-            if (ammo > 0) then
+            local card = CanadaCard:New(id)
+            local ammo = getAmmoManager(card)
+            if (ammo.ammo_system_remaining > 0) then
                 add_projectile("mods/copis_gun/files/entities/projectiles/silver_magnum.xml")
                 c.screenshake = c.screenshake + 0.2
                 c.damage_critical_chance = c.damage_critical_chance + 20
-                CardSetAmmo(card, ammo - 1)
+                ammo.ammo_system_remaining = ammo.ammo_system_remaining - 1
+            else
+                local x, y = EntityGetTransform(GetUpdatedEntityID())
+                GamePlaySound( "mods/copis_gun/files/audio/Copis_Gun.bank", "9mm/blank", x, y )
             end
         end,
     },
+--[[
+    {
+        id                  = "COPIS_GUN_ROSE_BULLET",
+        name                = "$action_name_copis_gun_rose_bullet",
+        description         = "$action_desc_copis_gun_rose_bullet",
+        sprite              = "mods/copis_gun/files/ui_gfx/gun_actions/rose_bullet.png",
+        related_projectiles = { "mods/copis_gun/files/entities/projectiles/rose_bullet.xml" },
+        type                = ACTION_TYPE_PROJECTILE,
+        spawn_level         = "0,0",
+        spawn_probability   = "0,0",
+        price               = 220,
+        mana                = 0,
+        max_uses            = 170,
+        custom_xml_file     = "mods/copis_gun/files/entities/misc/custom_cards/rose_bullet.xml",
+        action              = function()
+            c.fire_rate_wait = c.fire_rate_wait + 10.8;
+            if reflecting then
+                Reflection_RegisterProjectile("mods/copis_gun/files/entities/projectiles/rose_bullet.xml")
+                Reflection_RegisterProjectile("mods/copis_gun/files/entities/projectiles/rose_bullet.xml")
+                Reflection_RegisterProjectile("mods/copis_gun/files/entities/projectiles/rose_bullet.xml")
+                return;
+            end
+            if (recursion_level or iteration) ~= nil then return; end
+
+            local card = CurrentCard()
+            local ammo = getAmmoManager(card)
+            if (ammo.ammo_system_remaining > 0) then
+                c.screenshake = c.screenshake + 0.1
+                c.damage_critical_chance = c.damage_critical_chance + 20
+                BeginProjectile("mods/copis_gun/files/entities/projectiles/burst_fire.xml");
+                    BeginTriggerTimer( 1 );
+                        BeginProjectile("mods/copis_gun/files/entities/projectiles/rose_bullet.xml"); EndProjectile();
+                        register_action( c );
+                        SetProjectileConfigs();
+                    EndTrigger();
+                    BeginTriggerTimer( 4 );
+                        BeginProjectile("mods/copis_gun/files/entities/projectiles/rose_bullet.xml"); EndProjectile();
+                        register_action( c );
+                        SetProjectileConfigs();
+                    EndTrigger();
+                    BeginTriggerTimer( 7 );
+                        BeginProjectile("mods/copis_gun/files/entities/projectiles/rose_bullet.xml"); EndProjectile();
+                        register_action( c );
+                        SetProjectileConfigs();
+                    EndTrigger();
+                EndProjectile()
+                ammo.ammo_system_remaining = ammo.ammo_system_remaining - 3
+            else
+                local x, y = EntityGetTransform(GetUpdatedEntityID())
+                GamePlaySound( "mods/copis_gun/files/audio/Copis_Gun.bank", "9mm/blank", x, y )
+            end
+        end,
+    },
+
+    {
+        id                  = "COPIS_GUN_ROSE_MAGNUM",
+        name                = "$action_name_copis_gun_rose_magnum",
+        description         = "$action_desc_copis_gun_rose_magnum",
+        sprite              = "mods/copis_gun/files/ui_gfx/gun_actions/rose_magnum.png",
+        related_projectiles = { "mods/copis_gun/files/entities/projectiles/rose_magnum.xml" },
+        type                = ACTION_TYPE_PROJECTILE,
+        spawn_level         = "0,0",
+        spawn_probability   = "0,0",
+        price               = 330,
+        mana                = 0,
+        max_uses            = 170,
+        custom_xml_file     = "mods/copis_gun/files/entities/misc/custom_cards/rose_magnum.xml",
+        action              = function()
+            c.fire_rate_wait = c.fire_rate_wait + 14.6;
+            if reflecting then
+                Reflection_RegisterProjectile("mods/copis_gun/files/entities/projectiles/rose_magnum.xml")
+                return;
+            end
+            if (recursion_level or iteration) ~= nil then return; end
+
+            local card = CurrentCard()
+            local ammo = getAmmoManager(card)
+            if (ammo.ammo_system_remaining > 0) then
+                add_projectile("mods/copis_gun/files/entities/projectiles/rose_magnum.xml")
+                c.screenshake = c.screenshake + 0.2
+                c.damage_critical_chance = c.damage_critical_chance + 20
+                ammo.ammo_system_remaining = ammo.ammo_system_remaining - 1
+            else
+                local x, y = EntityGetTransform(GetUpdatedEntityID())
+                GamePlaySound( "mods/copis_gun/files/audio/Copis_Gun.bank", "9mm/blank", x, y )
+            end
+        end,
+    },]]
 }
 
 for k, v in ipairs(to_insert) do
--- init.lua
+++ init.lua
@@ -1,11 +1,10 @@
-dofile_once("mods/copis_gun/CANADA_lib/init.lua").init("mods/copis_gun/CANADA_lib")
-ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/copis_gun/files/scripts/gun/actions.lua" )
+dofile_once("mods/copis_gun/CANADA_lib/init.lua").init("mods/copis_gun/CANADA_lib")                     -- CANADA initer
+ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/copis_gun/files/scripts/gun/actions.lua" )  -- add bullets
+ModRegisterAudioEventMappings("mods/copis_gun/files/audio/GUIDs.txt")
 
-
-function OnPlayerSpawned( player_entity ) -- This runs when player entity has been created
+function OnPlayerSpawned( player_entity )           -- This runs when player entity has been created
 	if not GameHasFlagRun("copis_gun_spawned") then
-        -- most based akimbo shooter sim
-		if ModIsEnabled("Twin-Linked") then
+		if ModIsEnabled("Twin-Linked") then         -- most based akimbo shooter sim
 			local x, y = EntityGetTransform(player_entity)
 			EntityLoad("mods/copis_gun/files/entities/items/glock.xml", x + 20, y - 5)
 			GamePrintImportant("The gods grant you a second gun!", "make sure to unequip the offhand gun when restocking!", "mods/copis_gun/files/ui_gfx/decoration/twin.png")
