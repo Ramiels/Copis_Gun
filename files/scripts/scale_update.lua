3f3ea4e0dae52f33c78c3e809ff194d94a23f8fa
Human <54731850+Ramiels@users.noreply.github.com>
2022-10-20 00:39:36 -0400

    fixed annoying nil warning

--- files/scripts/scale_update.lua
+++ files/scripts/scale_update.lua
@@ -1,6 +1,6 @@
 local entity_id = GetUpdatedEntityID()
 local _, _, rad = EntityGetTransform(entity_id)
-local sprite_comp = EntityGetComponentIncludingDisabled(entity_id, "SpriteComponent", "item")
+local sprite_comp = EntityGetComponentIncludingDisabled(entity_id, "SpriteComponent", "item") or 0
 for i=1, #sprite_comp do
     ComponentSetValue2(sprite_comp[i], "special_scale_y", (rad >= -math.pi / 2 and rad <= math.pi / 2) and 0.4 or -0.4)
 end
